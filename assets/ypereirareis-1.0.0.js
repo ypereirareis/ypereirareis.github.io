4c6426fb28bd8905874eaf73efcbde3c2032e659
Yannick Pereira-Reis <yp@kptivestudio.com>
2015-03-05 09:26:00 +0100

    Google analytics

--- assets/ypereirareis-1.0.0.js
+++ assets/ypereirareis-1.0.0.js
@@ -1,5 +1,5 @@
 /*!
- * bernardopacheco - v0.1.0 - 2015-03-03
+ * bernardopacheco - v0.1.0 - 2015-03-05
  * Bernardo Pacheco
  * Source code for Bernardo Pacheco website
  * http://bernardopacheco.net
