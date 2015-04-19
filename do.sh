6f8523a431b29a13e3a1b8e94708e16e40802d58
Yannick PEREIRA-REIS <ypereirareis@users.noreply.github.com>
2015-04-19 16:22:30 +0200

    do-file to build Jekyll project

--- do.sh
+++ do.sh
@@ -9,6 +9,7 @@ JAUNE="\\033[1;33m"
 CYAN="\\033[1;36m"
 
 IMAGE_NAME='docker-ypereirareis'
+CONTAINER_NAME="jekyll-ypereirareis"
 
 log() {
   echo -e "$BLEU > $1 $NORMAL"
@@ -25,7 +26,7 @@ build() {
   [ $? != 0 ] && error "Docker image build failed !" && exit 100
 }
 
-install() {
+npm() {
   log "NPM install"
   docker run -it --rm -v $(pwd):/app $IMAGE_NAME npm install
   [ $? != 0 ] && error "Npm install failed !" && exit 101
@@ -51,13 +52,14 @@ grunt() {
 
 jkserve() {
   log "Jekyll serve"
-  docker run -it --rm -p 4000:4000 -v $(pwd):/app $IMAGE_NAME jekyll serve -H 0.0.0.0
+  docker run -it -d --name="$CONTAINER_NAME" -p 4000:4000 -v $(pwd):/app $IMAGE_NAME jekyll serve -H 0.0.0.0
   [ $? != 0 ] && error "Jekyll serve failed !" && exit 105
 }
 
-all() {
-  echo "Installing full application at once  "
-  install
+install() {
+  echo "Installing full application at once"
+  log "Removing previous container $CONTAINER_NAME" && docker rm -f $CONTAINER_NAME &> /dev/null || true
+  npm
   bower
   jkbuild
   grunt
@@ -69,23 +71,33 @@ bash() {
   docker run -it --rm -v $(pwd):/app $IMAGE_NAME bash
 }
 
+stop() {
+  docker stop $CONTAINER_NAME
+}
+
+start() {
+  docker start $CONTAINER_NAME
+}
+
+
 help() {
   echo "-----------------------------------------------------------------------"
   echo "                      Available commands                              -"
   echo "-----------------------------------------------------------------------"
   echo -e -n "$VERT"
   echo "   > build - To build the Docker image"
-  echo "   > install - To install NPM modules/deps"
+  echo "   > npm - To install NPM modules/deps"
   echo "   > bower - To install Bower/Js deps"
   echo "   > jkbuild - To build Jekyll project"
   echo "   > grunt - To run grunt task"
   echo "   > jkserve - To serve the project/blog on 127.0.0.1:4000"
-  echo "   > all - To execute full install at once"
+  echo "   > install - To execute full install at once"
+  echo "   > stop - To stop main jekyll container"
+  echo "   > start - To start main jekyll container"
   echo "   > bash - Log you into container"
   echo "   > help - Display this help"
-  echo "-----------------------------------------------------------------------"
   echo -e -n "$NORMAL"
-
+  echo "-----------------------------------------------------------------------"
 
 }
 
