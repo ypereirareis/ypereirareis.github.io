322cfde16dd7dbf4a36964b6b1e4bc5431522eb3
Yannick Pereira-Reis <yannick@zol.fr>
2015-10-02 17:27:19 +0200

    ovh rescue mode

--- assets/ypereirareis-1.0.0.js
+++ assets/ypereirareis-1.0.0.js
@@ -1,5 +1,5 @@
 /*!
- * ypereirareis - v0.1.0 - 2015-05-29
+ * ypereirareis - v0.1.0 - 2015-10-02
  * Yannick PEREIRA-REIS
  * Source code for Yannick PEREIRA-REIS website
  * http://ypereirareis.github.io
