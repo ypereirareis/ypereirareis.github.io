a320632eadee6070464ddf1917e8be7254991ea2
Yannick Pereira-Reis <yannick@zol.fr>
2015-10-23 13:02:47 +0200

    Gulpfile merge css, less, sass

--- assets/ypereirareis-1.0.0.js
+++ assets/ypereirareis-1.0.0.js
@@ -1,5 +1,5 @@
 /*!
- * ypereirareis - v0.1.0 - 2015-10-22
+ * ypereirareis - v0.1.0 - 2015-10-23
  * Yannick PEREIRA-REIS
  * Source code for Yannick PEREIRA-REIS website
  * http://ypereirareis.github.io
