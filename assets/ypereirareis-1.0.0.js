2e11cc05f0b7c864d8b50feb40ac58d454650260
Yannick Pereira-Reis <yannick@zol.fr>
2016-02-10 14:33:13 +0100

    Symfony deprecation detector upgrade fixer

--- assets/ypereirareis-1.0.0.js
+++ assets/ypereirareis-1.0.0.js
@@ -1,5 +1,5 @@
 /*!
- * ypereirareis - v0.1.0 - 2016-02-05
+ * ypereirareis - v0.1.0 - 2016-02-10
  * Yannick PEREIRA-REIS
  * Source code for Yannick PEREIRA-REIS website
  * http://ypereirareis.github.io
